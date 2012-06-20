4e49372c1af7684ec89a7210e955966f5ea7371e
Kunj Karia <kunj.karia@gmail.com>
2012-06-20 09:19:15 +0530

    basic code added

--- KudoEdit/edit1.py
+++ KudoEdit/edit1.py
@@ -23,7 +23,7 @@ class Window(QtGui.QMainWindow):
         saveclick = QAction('Save', self)        
         saveclick.setShortcut('Ctrl+S')
         saveclick.setStatusTip('Save')
-        saveclick.triggered.connect(self.savetext)
+        saveclick.triggered.connect(self.savefile)
         
         exitclick = QtGui.QAction('Exit', self)        
         exitclick.setShortcut('Ctrl+Q')
@@ -39,6 +39,10 @@ class Window(QtGui.QMainWindow):
         openclick.setShortcut('Ctrl+O')
         openclick.setStatusTip('Open')
         openclick.triggered.connect(self.openfile)
+
+        saveasclick = QtGui.QAction('SaveAs', self)        
+        saveasclick.setStatusTip('Save As')
+        saveasclick.triggered.connect(self.save_asfile)
         
         self.textEdit = QtGui.QTextEdit()
         self.setCentralWidget(self.textEdit)
@@ -51,14 +55,28 @@ class Window(QtGui.QMainWindow):
         fileMenu.addAction(saveclick)
         fileMenu.addAction(exitclick)
         fileMenu.addAction(openclick)
+        fileMenu.addAction(saveasclick)
         
         self.showMaximized()        
         self.show()
                 
+    def savefile(self):
+        f=open(self.filename, 'w')
+        f.write(self.textEdit.toPlainText())
+        f.close()
+                
+    def save_asfile(self):
+        filename = QFileDialog.getSaveFileName(self,"Save File",os.getcwd())
+        self.filename=filename
+        print filename
+        f=open(filename, 'w')
+        f.write(self.textEdit.toPlainText())
+        f.close()
+        
     def openfile(self):
         filename = QFileDialog.getOpenFileName(self,"Open File",os.getcwd())
         print filename
-        f=open(filename,'r')
+        f=open(filename, 'r')
         text=f.read()
         f.close()
         self.textEdit.setText(text)
@@ -66,7 +84,7 @@ class Window(QtGui.QMainWindow):
     
     def newfile(self):
         filename = open('filename_temp','w')
-    
+
     def savetext(self):
         text=self.textEdit.toPlainText()
         if text:
@@ -74,12 +92,9 @@ class Window(QtGui.QMainWindow):
             now1=str(now)[:19]
             savefile=open(now1+'.txt','w')
             savefile.write(text)
-    
+            
     def closeEvent(self, event):
-        
-        reply = QtGui.QMessageBox.question(self, 'Message',
-            "Are you sure you want to quit?", QtGui.QMessageBox.Yes | 
-            QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+        reply = QtGui.QMessageBox.question(self, 'Message', "Are you sure you want to quit?", QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.No)
 
         if reply == QtGui.QMessageBox.Yes:
             event.accept()
